0cb79c0b97f5d0e67e405a97b9147a57540ea525
tazprince <tazbirkhanprince@gmail.com>
2026-09-26 01:55:26 +0600

    my-plan section add

---
+++ src/app/my-plan/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PlanDetails from "../../components/my-plan/PlanDetails";
+
+
+const MyPlanPage = () => {
+    return (
+        <PlanDetails />
+    );
+};
+
+export default MyPlanPage;
---
+++ src/components/my-plan/PlanDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlanDetails = () => {
+    return (
+        <div>
+            Hlw Plan
+        </div>
+    );
+};
+
+export default PlanDetails;
