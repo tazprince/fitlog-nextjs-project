4790475e794b57c26ec414badf4aa648aa1b20d4
tazprince <tazbirkhanprince@gmail.com>
2026-09-26 19:57:31 +0600

    Add Environment Variable For API

--- src/lib/api.jsx
+++ src/lib/api.jsx
@@ -2,13 +2,17 @@
 // For all the data
 
 const getWorkouts = async () => {
-    const response = await fetch('https://api.abcz.workers.dev/api/fitlog',{
-        cache:"no-store",
-    });
-    if(!response.ok){
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}`,
+        {
+            cache: "no-store",
+        }
+    );
+
+    if (!response.ok) {
         throw new Error("Failed To Fetch");
-        
     }
+
     const data = await response.json();
     return data;
 };
--- src/lib/singleapi.jsx
+++ src/lib/singleapi.jsx
@@ -1,13 +1,17 @@
 //For get 1 id's data
 
 const getWorkoutById = async (id) => {
-    const response = await fetch(`https://api.abcz.workers.dev/api/fitlog/${id}`,{
-         cache:"no-store",
-    });
-    if(!response.ok){
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/${id}`,
+        {
+            cache: "no-store",
+        }
+    );
+
+    if (!response.ok) {
         throw new Error("Failed To Fetch a Id");
-        
     }
+
     const data = await response.json();
     return data;
 };
